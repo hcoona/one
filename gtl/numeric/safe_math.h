bbd7852f7b5674d62d5959bf48c455a95c2c3ddd
Shuai Zhang <zhangshuai.ustc@gmail.com>
2020-07-04 23:34:20 +0800

    [GTL] Add numeric.

---
+++ gtl/numeric/safe_math.h
@@ -0,0 +1,12 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GTL_NUMERIC_SAFE_MATH_H_
+#define GTL_NUMERIC_SAFE_MATH_H_
+
+#include "gtl/numeric/checked_math.h"
+#include "gtl/numeric/clamped_math.h"
+#include "gtl/numeric/safe_conversions.h"
+
+#endif  // GTL_NUMERIC_SAFE_MATH_H_
