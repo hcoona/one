22da302494ea3c86ca0f45712ac01d16f6c54fc2
Shuai Zhang <zhangshuai.ustc@gmail.com>
2020-07-11 22:21:14 +0800

    [DirManifest] Calculate SHA256 for files.

--- directory_manifest/generator.cc
+++ directory_manifest/generator.cc
@@ -5,12 +5,14 @@
 #include <vector>
 
 #include "absl/status/status.h"
+#include "absl/strings/escaping.h"
 #include "absl/strings/str_cat.h"
 #include "absl/time/time.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
 #include "directory_manifest/macros.h"
 #include "gtl/container/circular_deque.h"
+#include "gtl/crypto/sha2.h"
 #include "gtl/file_statistics.h"
 #include "gtl/file_system.h"
 
@@ -47,10 +49,11 @@ absl::Status Generator::Init() {
 absl::Status Generator::Run() { return Visit(root_directory_); }
 
 absl::Status Generator::Visit(std::string root) {
+  absl::Status s;
   CircularQueue<std::string> visit_queue;
   visit_queue.emplace(std::move(root));
 
-  // TODO(zhangshuai.ustc): Multi-threading to boost it.
+  // TODO(zhangshuai.ustc): Multi-threading to boost the enumeration.
   while (!visit_queue.empty()) {
     std::string current = visit_queue.front();
     visit_queue.pop();
@@ -63,7 +66,7 @@ absl::Status Generator::Visit(std::string root) {
       std::string path = file_system_->JoinPath(current, filename);
 
       gtl::FileStatistics stat;
-      absl::Status s = file_system_->Stat(path, &stat);
+      s = file_system_->Stat(path, &stat);
       if (!s.ok()) {
         LOG(WARNING) << "Failed to get file statistics for " << path;
         continue;
@@ -75,6 +78,25 @@ absl::Status Generator::Visit(std::string root) {
               << ", is_directory=" << stat.is_directory << "}";
       if (stat.is_directory) {
         visit_queue.emplace(std::move(path));
+      } else {
+        // TODO(zhangshuai.ustc): Post task into thread pool.
+        std::unique_ptr<gtl::ReadOnlyMemoryRegion> memory_region;
+        s = file_system_->NewReadOnlyMemoryRegionFromFile(path, &memory_region);
+        if (!s.ok()) {
+          LOG(ERROR) << "Failed to read file '" << path
+                     << "': " << s.ToString();
+          continue;
+        }
+
+        std::array<uint8_t, gtl::crypto::kSHA256Length> sha256_bytes =
+            gtl::crypto::SHA256Hash(absl::MakeSpan(
+                reinterpret_cast<const uint8_t*>(memory_region->data()),
+                memory_region->length()));
+        std::string sha256_hex_string = absl::BytesToHexString(
+            absl::string_view(reinterpret_cast<const char*>(&sha256_bytes[0]),
+                              sha256_bytes.size()));
+        // TODO(zhangshuai.ustc): Write to tables.
+        LOG(INFO) << "\"" << path << "\",\"" << sha256_hex_string << "\"";
       }
     }
   }
