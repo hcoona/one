b9a009df45f42cdee3123420ccbe133d325ee43c
Shuai Zhang <zhangshuai.ustc@gmail.com>
2020-07-13 20:36:29 +0800

    [DirManifest] (WIP) Add thread pool.

---
+++ directory_manifest/thread_pool.h
@@ -0,0 +1,14 @@
+#ifndef DIRECTORY_MANIFEST_THREAD_POOL_H_
+#define DIRECTORY_MANIFEST_THREAD_POOL_H_
+
+#include "gtl/localtion.h"
+
+namespace hcoona {
+
+class Task {};
+
+class ThreadGroup {};
+
+}  // namespace hcoona
+
+#endif  // DIRECTORY_MANIFEST_THREAD_POOL_H_
