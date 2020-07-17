af5c8e2a90cba6db7080032d4416d0b010765d61
Shuai Zhang <zhangshuai.ustc@gmail.com>
2020-07-17 18:46:54 +0800

    [Pb2Arrow] Add Int32 test.

--- codelab/pb_to_arrow/converter_test.cc
+++ codelab/pb_to_arrow/converter_test.cc
@@ -76,6 +76,68 @@ TEST(TestDouble, TestDoubleMap) {
   EXPECT_TRUE(s.ok()) << s.ToString();
 }
 
+TEST(TestInt32, TestInt32Value) {
+  ClearArrowArrayBuilderDebugContext();
+
+  Int32Wrapper int32_wrapper;
+  int32_wrapper.set_int32_value(3);
+  google::protobuf::Message* message = &int32_wrapper;
+  absl::Span<const google::protobuf::Message* const> messages =
+      absl::MakeConstSpan(
+          absl::implicit_cast<google::protobuf::Message**>(&message), 1);
+
+  arrow::MemoryPool* pool = arrow::default_memory_pool();
+  std::shared_ptr<arrow::Table> table;
+  absl::Status s =
+      ConvertTable(*(message->GetDescriptor()), messages, pool, &table);
+  ASSERT_TRUE(s.ok()) << s.ToString();
+
+  s = FromArrowStatus(table->ValidateFull());
+  EXPECT_TRUE(s.ok()) << s.ToString();
+}
+
+TEST(TestInt32, TestInt32List) {
+  ClearArrowArrayBuilderDebugContext();
+
+  Int32ListWrapper int32_list_wrapper;
+  int32_list_wrapper.add_int32_list(3);
+  int32_list_wrapper.add_int32_list(5);
+  google::protobuf::Message* message = &int32_list_wrapper;
+  absl::Span<const google::protobuf::Message* const> messages =
+      absl::MakeConstSpan(
+          absl::implicit_cast<google::protobuf::Message**>(&message), 1);
+
+  arrow::MemoryPool* pool = arrow::default_memory_pool();
+  std::shared_ptr<arrow::Table> table;
+  absl::Status s =
+      ConvertTable(*(message->GetDescriptor()), messages, pool, &table);
+  ASSERT_TRUE(s.ok()) << s.ToString();
+
+  s = FromArrowStatus(table->ValidateFull());
+  EXPECT_TRUE(s.ok()) << s.ToString();
+}
+
+TEST(TestInt32, TestInt32Map) {
+  ClearArrowArrayBuilderDebugContext();
+
+  Int32MapWrapper int32_map_wrapper;
+  int32_map_wrapper.mutable_int32_map()->operator[](3) = 3;
+  int32_map_wrapper.mutable_int32_map()->operator[](5) = 5;
+  google::protobuf::Message* message = &int32_map_wrapper;
+  absl::Span<const google::protobuf::Message* const> messages =
+      absl::MakeConstSpan(
+          absl::implicit_cast<google::protobuf::Message**>(&message), 1);
+
+  arrow::MemoryPool* pool = arrow::default_memory_pool();
+  std::shared_ptr<arrow::Table> table;
+  absl::Status s =
+      ConvertTable(*(message->GetDescriptor()), messages, pool, &table);
+  ASSERT_TRUE(s.ok()) << s.ToString();
+
+  s = FromArrowStatus(table->ValidateFull());
+  EXPECT_TRUE(s.ok()) << s.ToString();
+}
+
 }  // namespace
 }  // namespace codelab
 }  // namespace hcoona
