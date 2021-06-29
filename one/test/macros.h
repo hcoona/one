a2c6fe225ff74467c670c38086f0c0498a728316
Shuai Zhang <zhangshuai.ustc@gmail.com>
2021-06-29 15:34:43 +0800

    [test] prefix macros with ONE_.

--- one/test/macros.h
+++ one/test/macros.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define ASSERT_STATUS_OK(status)  \
-  do {                            \
-    const auto& __s = (status);   \
-    ASSERT_TRUE(__s.ok()) << __s; \
+#define ONE_ASSERT_STATUS_OK(status) \
+  do {                               \
+    const auto& __s = (status);      \
+    ASSERT_TRUE(__s.ok()) << __s;    \
   } while (false)
