cde5277b718a46906b6310a1a85d64dab2449a72
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-03-06 16:17:54 +0800

    Chore: add setup_clang.sh & update .bazelrc

---
+++ tools/setup_clang.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Copyright (c) 2022 Zhang Shuai<zhangshuai.ustc@gmail.com>.
+# All rights reserved.
+#
+# This file is part of ONE.
+#
+# ONE is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# ONE is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# ONE. If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright 2020 Envoyproxy Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copied from https://github.com/envoyproxy/envoy/blob/55e9b316d2e7d17a05b68924d0dffc2635ab8e52/bazel/setup_clang.sh
+#
+# Modifications:
+#
+# 1. Remove action_env ENVOY_UBSAN_VPTR=1
+
+BAZELRC_FILE="${BAZELRC_FILE:-$(bazel info workspace)/clang.bazelrc}"
+
+LLVM_PREFIX=$1
+
+if [[ ! -e "${LLVM_PREFIX}/bin/llvm-config" ]]; then
+  echo "Error: cannot find llvm-config in ${LLVM_PREFIX}."
+  exit 1
+fi
+
+PATH="$("${LLVM_PREFIX}"/bin/llvm-config --bindir):${PATH}"
+export PATH
+
+RT_LIBRARY_PATH="$(dirname "$(find "$(llvm-config --libdir)" -name libclang_rt.ubsan_standalone_cxx-x86_64.a | head -1)")"
+
+echo "# Generated file, do not edit. If you want to disable clang, just delete this file.
+build:clang --action_env='PATH=${PATH}'
+build:clang --action_env=CC=clang
+build:clang --action_env=CXX=clang++
+build:clang --action_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
+build:clang --repo_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
+build:clang --linkopt='-L$(llvm-config --libdir)'
+build:clang --linkopt='-Wl,-rpath,$(llvm-config --libdir)'
+build:clang-asan --copt=-fsanitize=vptr,function
+build:clang-asan --linkopt=-fsanitize=vptr,function
+build:clang-asan --linkopt='-L${RT_LIBRARY_PATH}'
+build:clang-asan --linkopt=-l:libclang_rt.ubsan_standalone-x86_64.a
+build:clang-asan --linkopt=-l:libclang_rt.ubsan_standalone_cxx-x86_64.a
+" > "${BAZELRC_FILE}"
