0b69f92d578ae9653e56d1aaa6c50ff116e8cebd
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-03-12 21:25:00 +0800

    Jinduo: port net/event_loop_test.cc

---
+++ one/jinduo/net/event_loop_test.cc
@@ -0,0 +1,65 @@
+// Copyright (c) 2022 Zhang Shuai<zhangshuai.ustc@gmail.com>.
+// All rights reserved.
+//
+// This file is part of ONE.
+//
+// ONE is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// ONE is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/muduo/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+#include "one/jinduo/net/event_loop.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <thread>
+
+#include "absl/time/time.h"
+#include "one/jinduo/base/this_thread.h"
+
+jinduo::net::EventLoop* g_loop;
+
+void callback() {
+  printf("callback(): pid = %d, tid = %d\n", getpid(),
+         jinduo::this_thread::tid());
+  jinduo::net::EventLoop anotherLoop;
+}
+
+void threadFunc() {
+  printf("threadFunc(): pid = %d, tid = %d\n", getpid(),
+         jinduo::this_thread::tid());
+
+  assert(jinduo::net::EventLoop::getEventLoopOfCurrentThread() == nullptr);
+  jinduo::net::EventLoop loop;
+  assert(jinduo::net::EventLoop::getEventLoopOfCurrentThread() == &loop);
+  loop.runAfter(absl::Seconds(1), callback);
+  loop.loop();
+}
+
+int main() {
+  printf("main(): pid = %d, tid = %d\n", getpid(), jinduo::this_thread::tid());
+
+  assert(jinduo::net::EventLoop::getEventLoopOfCurrentThread() == nullptr);
+  jinduo::net::EventLoop loop;
+  assert(jinduo::net::EventLoop::getEventLoopOfCurrentThread() == &loop);
+
+  std::thread thread(threadFunc);
+
+  loop.loop();
+
+  thread.join();
+}
