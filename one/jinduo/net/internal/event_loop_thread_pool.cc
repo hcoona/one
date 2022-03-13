39e0d6d447dbbde0adf3ee384468c0135b2fe856
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-03-13 23:58:37 +0800

    Jinduo: port internal/event_loop_thread_pool.h

---
+++ one/jinduo/net/internal/event_loop_thread_pool.cc
@@ -0,0 +1,100 @@
+// Copyright (c) 2022 Zhang Shuai<zhangshuai.ustc@gmail.com>.
+// All rights reserved.
+//
+// This file is part of ONE.
+//
+// ONE is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// ONE is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// ONE. If not, see <https://www.gnu.org/licenses/>.
+//
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/muduo/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+#include "one/jinduo/net/internal/event_loop_thread_pool.h"
+
+#include <stdio.h>
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "absl/base/casts.h"
+#include "absl/container/fixed_array.h"
+#include "one/jinduo/net/event_loop.h"
+#include "one/jinduo/net/event_loop_thread.h"
+
+namespace jinduo {
+namespace net {
+
+EventLoopThreadPool::EventLoopThreadPool(EventLoop* baseLoop,
+                                         std::string nameArg)
+    : baseLoop_(baseLoop), name_(std::move(nameArg)) {}
+
+void EventLoopThreadPool::start(const ThreadInitCallback& cb) {
+  assert(!started_);
+  baseLoop_->assertInLoopThread();
+
+  started_ = true;
+
+  for (int i = 0; i < numThreads_; ++i) {
+    absl::FixedArray<char> buf(name_.size() + 32);
+    snprintf(buf.data(), buf.size(), "%s%d", name_.c_str(), i);
+    auto* t = new EventLoopThread(cb, buf.data());
+    threads_.emplace_back(t);
+    loops_.emplace_back(t->startLoop());
+  }
+  if (numThreads_ == 0 && cb) {
+    cb(baseLoop_);
+  }
+}
+
+EventLoop* EventLoopThreadPool::getNextLoop() {
+  baseLoop_->assertInLoopThread();
+  assert(started_);
+  EventLoop* loop = baseLoop_;
+
+  if (!loops_.empty()) {
+    // round-robin
+    loop = loops_[next_];
+    ++next_;
+    if (absl::implicit_cast<size_t>(next_) >= loops_.size()) {
+      next_ = 0;
+    }
+  }
+  return loop;
+}
+
+EventLoop* EventLoopThreadPool::getLoopForHash(size_t hashCode) {
+  baseLoop_->assertInLoopThread();
+  EventLoop* loop = baseLoop_;
+
+  if (!loops_.empty()) {
+    loop = loops_[hashCode % loops_.size()];
+  }
+  return loop;
+}
+
+std::vector<EventLoop*> EventLoopThreadPool::getAllLoops() {
+  baseLoop_->assertInLoopThread();
+  assert(started_);
+  if (loops_.empty()) {
+    return {baseLoop_};
+  }
+  return loops_;
+}
+
+}  // namespace net
+}  // namespace jinduo
---
+++ one/jinduo/net/internal/event_loop_thread_pool.h
@@ -0,0 +1,83 @@
+// Copyright (c) 2022 Zhang Shuai<zhangshuai.ustc@gmail.com>.
+// All rights reserved.
+//
+// This file is part of ONE.
+//
+// ONE is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// ONE is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// ONE. If not, see <https://www.gnu.org/licenses/>.
+//
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/muduo/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+//
+// This is an internal header file, you should not include this.
+
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace jinduo {
+namespace net {
+
+class EventLoop;
+class EventLoopThread;
+
+class EventLoopThreadPool {
+ public:
+  using ThreadInitCallback = std::function<void(EventLoop*)>;
+
+  EventLoopThreadPool(EventLoop* baseLoop, std::string nameArg);
+  ~EventLoopThreadPool() = default;
+
+  // Disallow copy.
+  EventLoopThreadPool(const EventLoopThreadPool&) noexcept = delete;
+  EventLoopThreadPool& operator=(const EventLoopThreadPool&) noexcept = delete;
+
+  // Allow move but not implemented yet.
+  EventLoopThreadPool(EventLoopThreadPool&&) noexcept = delete;
+  EventLoopThreadPool& operator=(EventLoopThreadPool&&) noexcept = delete;
+
+  void setThreadNum(int numThreads) { numThreads_ = numThreads; }
+  void start(const ThreadInitCallback& cb = ThreadInitCallback());
+
+  // valid after calling start()
+  /// round-robin
+  EventLoop* getNextLoop();
+
+  /// with the same hash code, it will always return the same EventLoop
+  EventLoop* getLoopForHash(size_t hashCode);
+
+  std::vector<EventLoop*> getAllLoops();
+
+  [[nodiscard]] bool started() const { return started_; }
+
+  [[nodiscard]] const std::string& name() const { return name_; }
+
+ private:
+  EventLoop* baseLoop_;
+  std::string name_;
+  bool started_{false};
+  int numThreads_{0};
+  int next_{0};
+  std::vector<std::unique_ptr<EventLoopThread>> threads_;
+  std::vector<EventLoop*> loops_;
+};
+
+}  // namespace net
+}  // namespace jinduo
