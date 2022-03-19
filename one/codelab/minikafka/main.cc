db8d08efd3fc666056e2e2848c5cba58d2755b5f
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-03-20 00:41:02 +0800

    Improvement: minikafka run multiple TCP servers & register signal handlers to event loop

--- one/codelab/minikafka/main.cc
+++ one/codelab/minikafka/main.cc
@@ -15,19 +15,23 @@
 // You should have received a copy of the GNU General Public License along with
 // ONE. If not, see <https://www.gnu.org/licenses/>.
 
+#include <csignal>
 #include <cstddef>
+#include <functional>
 #include <limits>
+#include <memory>
+#include <thread>
 
 #include "absl/base/casts.h"
+#include "absl/functional/bind_front.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
 #include "one/codelab/minikafka/core/kafka_service.h"
 #include "one/codelab/minikafka/transport/kafka_tcp_server.h"
 #include "one/jinduo/net/event_loop.h"
 #include "one/jinduo/net/event_loop_thread.h"
-
-DEFINE_uint32(port, 0, "Kafka plain protocol binding port.");
-DEFINE_uint32(inspector_port, 0, "Inspector HTTP server binding port.");
+#include "one/jinduo/net/event_loop_thread_pool.h"
+#include "one/jinduo/net/signal_handler_manager.h"
 
 namespace {
 
@@ -36,7 +40,31 @@ bool ValidateTcpPort(const char* /*flag_name*/, uint32_t value) {
          absl::implicit_cast<uint32_t>(std::numeric_limits<uint16_t>::max());
 }
 
+void HandleSignal(
+    jinduo::net::EventLoop* loop,
+    hcoona::minikafka::KafkaService* kafka_service,
+    std::vector<hcoona::minikafka::KafkaTcpServer*>* kafka_tcp_servers,
+    int signum) {
+  CHECK(signum == SIGTERM || signum == SIGINT);
+
+  LOG(WARNING) << "SIGTERM|SIGINT received, exiting...";
+
+  // The events are drained during event loop quiting.
+  loop->QueueInLoop(absl::bind_front(&jinduo::net::EventLoop::Quit, loop));
+
+  // Child loops exited during kafka tcp server stopping.
+  for (auto&& s : *kafka_tcp_servers) {
+    s->Stop();
+  }
+
+  // TODO(zhangshuai.ustc): graceful shutdown the service.
+  (void)kafka_service;
+}
+
 }  // namespace
+
+DEFINE_uint32(port, 0, "Kafka plain protocol binding port.");
+DEFINE_uint32(inspector_port, 0, "Inspector HTTP server binding port.");
 
 DEFINE_validator(port, &ValidateTcpPort);
 DEFINE_validator(inspector_port, &ValidateTcpPort);
@@ -46,22 +74,41 @@ int main(int argc, char* argv[]) {
   google::InstallFailureSignalHandler();
   gflags::ParseCommandLineFlags(&argc, &argv, /*remove_flags=*/true);
 
-  // TODO(zhangshuai.ustc): Turn signal into an event to help stop the event
-  // loop gracefully.
-
   // TODO(zhangshuai.ustc): Bind a HTTP server to inspect the state of the
   // service.
 
-  // TODO(zhangshuai.ustc): Launch multiple TCP servers binding on the same port
-  // to process accept in parallel.
-
   jinduo::net::EventLoop loop;
-  hcoona::minikafka::KafkaService kafka_service;
-  hcoona::minikafka::KafkaTcpServer kafka_tcp_server(
-      &kafka_service, &loop, jinduo::net::InetAddress(FLAGS_port));
-  kafka_tcp_server.Start();
+  jinduo::net::EventLoopThreadPool event_loop_thread_pool(&loop, "minikafka");
+  event_loop_thread_pool.set_thread_num(
+      static_cast<int>(std::thread::hardware_concurrency()));
 
-  LOG(INFO) << "Binding at " << kafka_tcp_server.GetIpPort();
+  hcoona::minikafka::KafkaService kafka_service;
+  std::vector<hcoona::minikafka::KafkaTcpServer*> kafka_tcp_servers;
+
+  jinduo::net::SignalHandlerManager signal_handler_manager(&loop);
+  signal_handler_manager.SetSignalCallback(
+      SIGINT, absl::bind_front(&HandleSignal, &loop, &kafka_service,
+                               &kafka_tcp_servers));
+  signal_handler_manager.SetSignalCallback(
+      SIGTERM, absl::bind_front(&HandleSignal, &loop, &kafka_service,
+                                &kafka_tcp_servers));
+
+  event_loop_thread_pool.Start();
+
+  kafka_tcp_servers.reserve(std::thread::hardware_concurrency());
+  for (jinduo::net::EventLoop* child_loop :
+       event_loop_thread_pool.all_loops()) {
+    auto kafka_tcp_server = std::make_shared<hcoona::minikafka::KafkaTcpServer>(
+        &kafka_service, child_loop, jinduo::net::InetAddress(FLAGS_port));
+    kafka_tcp_servers.emplace_back(kafka_tcp_server.get());
+    child_loop->RunInLoop([s = kafka_tcp_server.get()] { s->Start(); });
+    // It's safe to store `std::shared_ptr` in `std::any`.
+    child_loop->set_context(std::move(kafka_tcp_server));
+  }
+
+  LOG(INFO) << "Minikafka service is running at port " << FLAGS_port;
 
   loop.Loop();
+
+  // TODO(zhangshuai.ustc): Join `kafka_service`.
 }
